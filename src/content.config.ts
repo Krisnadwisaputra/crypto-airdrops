6d5311d8121bbfdf3005af0ff5e9eb82f4362aa9
Krisnadwisaputra <skrisnadwi329@gmail.com>
2026-09-17 20:04:12 +0700

    Finalize Astro migration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,13 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const guides = defineCollection({
-  type: "content",
+  loader: glob({
+    pattern: "**/*.md",
+    base: "./src/content/guides"
+  }),
+
   schema: z.object({
     title: z.string(),
     description: z.string(),
